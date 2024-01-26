7c627ed84fc3d9b62758c0cd9b5ebdf5b8529e00
Zach Burns <124537298+ZBurnsz@users.noreply.github.com>
2024-01-26 16:50:28 -0600

    nnn

--- CS2050/Labs/Lab1/Lab1.c
+++ CS2050/Labs/Lab1/Lab1.c
@@ -1,11 +1,16 @@
-#include "Lab1.h"
+#include "lab1.h"
 
+
+//swap function 
 void swap(int *a, int *b) {
     int temp = *a;
     *a = *b;
     *b = temp;
+    //used 8_21 lab for looking at swapping with pointers 
 }
 
+
+//finding min and max  
 int swapMaxMin(int array[], int size) {
     
     int max = 0;
@@ -19,18 +24,20 @@ int swapMaxMin(int array[], int size) {
             min = i;
         }
     }
-
+    //swapping of the min and max 
     swap(&array[min], &array[max]); 
 
     return 0;
 }
-
+//finding last character funciton 
 int lastChar(char str[], char *result) {
 
+    //nothing is there 
     if (str[0] == '\0') {
         return 1;
     }
 
+    //finding end character
     int i = 0;
     while (str[i] != '\0') {
         *result = str[i];

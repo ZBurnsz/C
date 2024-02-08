41e37937e9cd04204b567f31357cc9c8e5acbbb4
Zach Burns <124537298+ZBurnsz@users.noreply.github.com>
2024-02-08 10:29:40 -0600

    bb

--- CS2050/Labs/Prelabs/prelab3/main.c
+++ CS2050/Labs/Prelabs/prelab3/main.c
@@ -2,16 +2,29 @@
 
 
 
-int main (void){
+void * createArray(int, int){
+
+    void * array = malloc(sizeof(int) * size);
+    return array;
+
+
+}
+
+int getArraySize(void*){
 
 
 
 
+    int size = sizeof(*array) / sizeof(int);
+    return size;
 
 
 
+}
 
+void freeArray (void*){
 
+free(*array);
+*array = NULL;
 
-    return 0; 
 }

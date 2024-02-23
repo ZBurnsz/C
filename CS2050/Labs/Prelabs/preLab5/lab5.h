d38518a00126869ffc0e2b13d41b6daaadc7f023
ZBurnsz <124537298+ZBurnsz@users.noreply.github.com>
2024-02-23 15:32:12 -0600

    lab5

--- CS2050/Labs/Prelabs/preLab5/lab5.h
+++ CS2050/Labs/Prelabs/preLab5/lab5.h
@@ -3,12 +3,17 @@
 #include <string.h>
 
 typedef struct {
-    char list[100];
-    int maxLength;
-    int length;
+    char **items;  
+    int maxLength; 
+    int length;    
 } GroceryList;
+
 
 GroceryList *GroceryListInit(int maxLength, int *errorCode);
 GroceryList *GroceryListAppend(GroceryList *glist, char *itemName, int *errorCode);
 char *GroceryListGetItem(GroceryList *glist, int k, int *errorCode);
 
+
+int GroceryCount(GroceryList *glist);
+void GroceryListPrint(GroceryList *glist);
+void GroceryListDelete(GroceryList *glist);

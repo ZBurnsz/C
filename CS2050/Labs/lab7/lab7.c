eea125ebf773bf4a8650bdc4bd76de14e90e4a0a
Zach Burns <124537298+ZBurnsz@users.noreply.github.com>
2024-03-08 16:05:07 -0600

    lab7

--- CS2050/Labs/lab7/lab7.c
+++ CS2050/Labs/lab7/lab7.c
@@ -4,8 +4,8 @@
 
 struct VendingMachineNode {
     StockItem *item; 
-    struct VendingMachine *next; 
-}VendingMachineNode; 
+    VendingMachine *next; 
+}; 
 
 struct VendingMachine_t{
     int numSlots; 
@@ -29,48 +29,53 @@ VendingMachine * newMachine(){
 }
 /*addStockItem 
 this funciton takes a a vendingMachine and an item and inserts that item into
-the next slto of the linked list. 
+the next slot of the linked list. 
 success = 1 
 failure = 0
 */
 int addStockItem(VendingMachine *vm, StockItem item){
 //assume no duplicates
-//dont need to sort the items 
-VendingMachine *newNode = (VendingMachine*)malloc(sizeof(VendingMachine));
+//dont need to sort the list 
 
-if (newNode == NUll){
-    return 0; 
-}else{
-    newNode-> = item; 
-    newNode->next = VendingMachine; 
+ struct VendingMachineNode *newNode = (struct VendingMachineNode*)malloc(sizeof(struct VendingMachineNode));
 
-
-}
-
-
-
+    newNode->item = newNode->item;  //probably not gonna work; 
+    newNode->next = vm->head; 
+    vm->head = newNode; 
+    vm->numSlots++; 
 
 }
+
 /*countExpensive:
 this function takes a vending machine and returns the number of items even if one is out of stock. 
 */
 int countItems(VendingMachine *vm){
 
+int count = 0; 
 
+struct VendingMachineNode *current = vm->head; 
+while (current != NULL){
+    count++;
+    current = current->next; 
+}
 
-
-
-
+return count;
 }
 /*countFull: 
 this function takes a vending machine list and returns the number of items that = maxStock 
 */
 int countFull(VendingMachine *vm){
+int count = 0; 
 
+struct VendingMachineNode *current = vm -> head; 
 
-
-
-
+while (current != NULL){
+    if (current->item->stock == current->item->maxStock){
+        count++; 
+    }
+    current = current->next; 
+}
+return count; 
 }
 /*getStockItem:
 this function takes a vending machine list, an ID of an item and returns that item to the result pointer 
@@ -80,17 +85,27 @@ return 0 = no item with the ID found
 int getStockItem(VendingMachine *vm, int ID, StockItem *result){
 
 
+struct VendingMachineNode *current = vm->head;
 
-
-
-    
+while (current != NULL){
+    if (current->item->ID == ID){
+        StockItem *result = current->item->ID;
+        return 1; 
+    }
+    current = current->next; 
+}
+    return 0; 
 }
 /*freeVendingMachine: 
 this function takes a vendingMachine and frees the memory allocated to it
 */
 void freeVendingMachine(VendingMachine *vm){
+struct VendingMachineNode *current = vm; 
+while(current != NULL){
+    struct VendingMachineNode *temp = current; 
+    current = current->next; 
+    free(temp);
+}
 
-
-
-
+return NULL; 
 }

ed4973e5ed66d6ad00e8e8559b72ef6223c75914
Zach Burns <124537298+ZBurnsz@users.noreply.github.com>
2024-03-08 15:39:45 -0600

    new

--- CS2050/Labs/Lab0/lab0.h
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
-int countOccurrences(int, int[], int);
--- CS2050/Prelabs/PreLab2/headers.h
+++
@@ -1,8 +0,0 @@
-#include <stdio.h> 
-#include <math.h>
-#include <stdlib.h>
-
-
-float * readFloatFileIntoArray(FILE *fptr, int *length);
-void freeFloatArray(float **floatArrayPtr);
-

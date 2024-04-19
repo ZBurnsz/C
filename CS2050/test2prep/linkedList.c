32ccc424441191d09305f03af3523008aa7bff69
ZBurnsz <124537298+ZBurnsz@users.noreply.github.com>
2024-04-19 16:05:27 -0600

    asd

---
+++ CS2050/test2prep/linkedList.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node {
+    int *data;
+    struct ListNode *next;
+} Node;
+
+
+void newList{
+
+Node newList = malloc(sizeof(Node));
+
+}
+
+void frontList{
+
+}
